9d0a4b48661f96f88353ce23dc447efe7188491f
Artem Gavrishchishen <artem.gavrishchishen@gmail.com>
2020-09-30 23:36:58 +0300

    initialize the project

---
+++ main.js
@@ -0,0 +1,17 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    backgroundColor: 'white',
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
