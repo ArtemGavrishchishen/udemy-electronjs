3f5f850a3dfce3dfca3db0a9ff1620e8aa8839fc
Artem Gavrishchishen <artem.gavrishchishen@gmail.com>
2020-10-08 00:00:31 +0300

    fix navbar views

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
+import Navbar from './components/Navbar';
 import HomeView from './views/Home';
 
 import routes from './configs/routes';
@@ -8,20 +9,23 @@ import routes from './configs/routes';
 const App = () => {
   return (
     <>
-      <Switch>
-        <Route exact path={routes.HOME} component={HomeView} />
-        <Route exact path={routes.SETTINGS}>
-          <h1>I am SETTINGS</h1>
-        </Route>
-        <Route exact path={routes.LOGIN}>
-          <h1>I am LOGIN</h1>
-        </Route>
-        <Route exact path={routes.REGISTER}>
-          <h1>I am REGISTER</h1>
-        </Route>
+      <Navbar />
+      <div className="content-wrapper">
+        <Switch>
+          <Route exact path={routes.HOME} component={HomeView} />
+          <Route exact path={routes.SETTINGS}>
+            <h1>I am SETTINGS</h1>
+          </Route>
+          <Route exact path={routes.LOGIN}>
+            <h1>I am LOGIN</h1>
+          </Route>
+          <Route exact path={routes.REGISTER}>
+            <h1>I am REGISTER</h1>
+          </Route>
 
-        <Redirect to="/" />
-      </Switch>
+          <Redirect to="/" />
+        </Switch>
+      </div>
     </>
   );
 };
